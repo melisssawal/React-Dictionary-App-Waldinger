927109f517128c465176614637ff231a49a1531f
melisssawal <mlope16@gmail.com>
2026-02-18 21:14:06 -0500

    Added axios. Troubleshooting vulnerabilities. Created the Dictionary api call and linked to the search.

--- src/SearchEngine.jsx
+++ src/SearchEngine.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import axios from "axios";
 
 
 function SearchEngine (){
@@ -8,13 +9,23 @@ function SearchEngine (){
     function wordSearch (event) {
 
         event.preventDefault();
-        alert (`Searching for ${keyword}`);
+
+
+        let apiKey = "34d34bfd03ebff0892b49ada97eo706t"
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+         axios.get(apiUrl).then(handleResponse);
     }
 
     function keywordChange (event) {
         setKeyword(event.target.value);
 
     }
+
+    function handleResponse (response) {
+        console.log(response);
+    }
+
+   
 
     return (
         <div className="Dictionary">
